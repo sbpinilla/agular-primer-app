9cc6e436b24e79eaad2a4e2cfe52c384075f9e64
sbpinilla <sbpinilla@hotmail.com>
2018-08-18 23:23:47 -0500

    Creacion de proyecto y primer componenete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {HelloWorldComponent} from './primercomponente/primer.component';
+import { HolaMundoComponent } from './hola-mundo/hola-mundo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent,
+    HolaMundoComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/primercomponente/primer.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+    selector: 'app-hello-world',
+    templateUrl : './primer.component.html',
+    styleUrls : ['./primer.component.css']
+
+})
+export class HelloWorldComponent {
+
+    title = 'Estas en angular (Y)';
+}
